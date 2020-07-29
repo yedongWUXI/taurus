a68fb2f45e10c48c521ff12919b12e94b405d07a
yedong <zhyd704@126.com>
2020-07-29 13:25:51 +0800

    增加docker部署

--- src/main/java/com/kaituo/comparison/back/core/config/Swagger2Config.java
+++ src/main/java/com/kaituo/comparison/back/core/config/Swagger2Config.java
@@ -2,6 +2,7 @@ package com.kaituo.comparison.back.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+@Profile({"dev", "test"})
 public class Swagger2Config {
     @Bean
     public Docket createRestApi() {
--- src/main/java/com/kaituo/comparison/back/core/controller/CommonControl.java
+++ src/main/java/com/kaituo/comparison/back/core/controller/CommonControl.java
@@ -1,8 +1,16 @@
 package com.kaituo.comparison.back.core.controller;
 
+import com.kaituo.comparison.back.common.bean.ResponseCode;
+import com.kaituo.comparison.back.common.bean.ResponseResult;
+import com.kaituo.comparison.back.core.mapper.shop.ShopMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Description:
@@ -11,10 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
  * @Modified by:
  */
 @RestController
+@Slf4j
 @RequestMapping(value = {"/common"})
 public class CommonControl {
+
+    @Autowired
+    ShopMapper shopMapper;
+
     @GetMapping(value = "/cors")
     public String cors() {
+        log.info("{}", "今天是个好日子~");
         return "cors success";
+    }
+
+    @GetMapping(value = "/test")
+    public ResponseResult test() {
+
+        List<Map<String, Object>> maps = shopMapper.selectAll();
+
+        return ResponseResult.e(ResponseCode.OK, maps);
     }
 }
